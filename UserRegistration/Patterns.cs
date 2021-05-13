f2a75ce4bc1af412a184e7fc6393bcc53e015f09
unknown <jimpidas>
2021-05-13 12:19:25 +0530

    [Jyosmita]:Added code to check if the password is minimum of 8 character

--- UserRegistration/Patterns.cs
+++ UserRegistration/Patterns.cs
@@ -10,6 +10,7 @@ namespace UserRegistration
         public static string Regex_name = "^[A-Z]{1}[a-z]{2,}$";
         public static string Regex_email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public static string Regex_phone = "^[1-9]{1}[0-9]{1}\\s[1-9]{1}[0-9]{9}$";
+        public static string Regex_password= "^[a-zA-Z0-9]{8,}$";
         public void IsValidFirstName()
         {
             Boolean flag;
@@ -83,6 +84,23 @@ namespace UserRegistration
         }
 
 
+
+        public void IsValidPasswordRule1()
+        {
+            Boolean flag;
+            Console.WriteLine("Enter the Password: minimum 8 character");
+            do
+            {
+                string Password = Console.ReadLine();
+                flag = (Regex.IsMatch(Password, Regex_password));
+                if (flag == true)
+                    Console.WriteLine("valid");
+                else
+                    Console.WriteLine("Invalid, Type again");
+
+            }
+            while (!flag);
+        }
 
 
 
