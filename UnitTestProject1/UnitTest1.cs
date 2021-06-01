f0273393268e614cd96bc35b01375a2bed72d34c
Jyosmita Das <33093330+jimpidas@users.noreply.github.com>
2021-06-01 23:40:59 +0530

    updated Unit test with Parametrized test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -47,5 +47,20 @@ namespace UnitTestProject1
             bool result = pattern.IsValidPasswordRule(Password);
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateSampleEmail_Should_return_true(string email)
+        {
+            Patterns pattern = new Patterns();
+            Assert.IsTrue(pattern.IsValidSampleEmail(email));
+        }
     }
 }
